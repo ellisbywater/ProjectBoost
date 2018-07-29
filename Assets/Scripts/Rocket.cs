7b4391a404f3f53f81c12d26ef079b548fe33a60
Ellis Garaudy <14321687+DevByWater@users.noreply.github.com>
2018-07-29 14:20:51 -0600

    rocket motions

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ProcessInput();
+	}
+
+	void ProcessInput(){
+		if(Input.GetKey(KeyCode.Space)){
+			rigidBody.AddRelativeForce(Vector3.up);
+		} 
+		if(Input.GetKey(KeyCode.A)){
+			transform.Rotate(Vector3.forward);
+		} else if(Input.GetKey(KeyCode.D)){
+			transform.Rotate(-Vector3.forward);
+		}
+	}
+}
